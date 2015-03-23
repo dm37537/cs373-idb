3962122fc72c6c744e8e3816405798f0ffcd6020
Kevin-Valle <kvalle@cs.utexas.edu>
2015-03-23 12:46:16 -0500

    Starting model unit tests

---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+from programmerJobs import app
+
+class ProgrammerJobsTestCase(unittest.TestCase)
+	
+	def testCreatingJob(self):
+		assert False
+	
+	def testModifyingJob(self):
+		assert False
+	
+	def testCreatingCompany(self):
+		assert False
+	
+	def testModifyingCompany(self):
+		assert False
+	
+	def testCreatingLocation(self):
+		assert False
+	
+	def testModifyingLocation(self):
+		assert False
+	
+	def testCreatingLanguage(self):
+		assert False
+	
+	def testModifyingLanguage(self):
+		assert False
+	
+	def testCreatingSkillset(self):
+		assert False
+	
+	def testModifyingSkillset(self):
+		assert False
+
+if __name__ == '__main__':
+	unittest.main()
