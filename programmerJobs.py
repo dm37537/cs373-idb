ebafaea1cd4fbb7fa7f3a1ebb919c4e76b33e97b
Da Meng <dameng@utexas.edu>
2015-03-26 18:12:15 -0500

    test

--- programmerJobs.py
+++ programmerJobs.py
@@ -47,7 +47,7 @@ def index():
 
 @app.route('/test')
 def test():
-	return send_from_directory('.', 'runTest.php')
+	return send_from_directory('.', 'runTest.html')
 
 # API
 @app.route('/api/job', methods=['GET'])
