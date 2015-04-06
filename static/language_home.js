a495a4d2e2aef38b88bdaa9fe6a76f9b5e821841
bsy6766 <bsy6766@gmail.com>
2015-04-06 11:49:59 -0500

    cleaned

--- static/language_home.js
+++ static/language_home.js
@@ -1,17 +1,6 @@
 $.getJSON("/api/rank", function(result){
 	var data = result["rank"];
-	// data = JSON.stringify(data);
-    // $.each(result, function(key, value){
-        // document.write(key+": "+value+"<br />"); 
-    //     document.getElementById("placeholder").innerHTML=data.language_ID+" "+data.language_name+" "+data.language_description;
-    // });
-	document.getElementById("language_0").innerHTML=JSON.stringify(data);
-	// document.getElementById("placeholder").innerHTML=JSON.stringify(data);
 	var len = data.length;
-	// document.getElementById("placeholder").innerHTML=JSON.stringify(first);
-	// var dataLen = data.length;
-    // $.each(data, function(key, value){
-    // var holder = "language_";
 
     //display rank
     var rank_holder = "rank_";
@@ -32,39 +21,16 @@ $.getJSON("/api/rank", function(result){
 			document.getElementById(rank_holder + rankId).innerHTML = "<a href=" + link + " >" + name + "</a>";
 		}
 
-		//display language icons
-
-		// var description = dict['language_description']
-		// if(name == "C/C++"){
-		// 	document.getElementById(holder+"C").innerHTML="C";
-		// 	document.getElementById(holder+"C++").innerHTML="C++";
-		// }
-		// else{
-		// 	document.getElementById(holder+name).innerHTML=name;
-		// }
-		// document.getElementById(rank_holder+rank_ID).innerHTML =
-    	// document.getElementById(holder+i).innerHTML=JSON.stringify(dict);
-	    // document.getElementById(holder+name).innerHTML=id + " " +name+ " " + description;
- 
-		// var id = dict["language_ID"];
-		// var name = dict['language_name'];
-		// var description = dict['language_description']
-	 //    document.getElementById("placeholder").innerHTML=id + " " +name+ " " + description;
-    }
-
-    //build table
+	}
 });
 
 $.getJSON("/api/language", function(result){
 	var data = result["languages"];
-	document.getElementById("language_1").innerHTML=JSON.stringify(data);
-	//display icons
 	var dataLen = data.length;
 	for(var i = 0; i < dataLen; ++i){
 		var dict = data[i];
 		var name = dict['language_name'];
 		var imgSrc = dict['language_image_small'];
-		// alert(imgSrc);
 
 		switch(name){
 			case "C/C++":
@@ -90,9 +56,8 @@ $.getJSON("/api/language", function(result){
 		htmlCode += "<a href=language/" + name + ">";
 		htmlCode += "<img src=\"static/" + imgSrc + "\" class=\"img-rounded\" style=\"margin: 30px\">";
 		htmlCode += "</a></div></div>";
-		// alert(htmlCode);
+
 		var icon_holder = "image_" + name;
-		// alert(icon_holder);
 		document.getElementById("image_" + name).innerHTML = htmlCode;
 	}
 });
