233fd624f4f2e3b94cbb384147560fbf8a9e6a9f
bsy6766 <bsy6766@gmail.com>
2015-04-06 19:07:33 -0500

    minor change with navbar, 2 new jobs for obj-c at Amazon

--- static/navbar.js
+++ static/navbar.js
@@ -1,4 +1,7 @@
 var langJson;
+var cmpyJson;
+var locJson;
+
 var state = document.getElementById("navbarParam").getAttribute("state");
 
 $.ajax({
@@ -11,12 +14,33 @@ $.ajax({
   }
 });
 
-var menuHTML = genLangMenu();
+$.ajax({
+  url: "/api/location",
+  async: false,
+  dataType: 'json',
+  success: function (response) {
+    // do stuff with response.
+    locJson = response["locations"];
+  }
+});
 
-document.getElementById("langMenu").innerHTML = menuHTML;
+$.ajax({
+  url: "/api/company",
+  async: false,
+  dataType: 'json',
+  success: function (response) {
+    // do stuff with response.
+    cmpyJson = response["companies"];
+  }
+});
+
+var len = langJson.length;
+
+document.getElementById("langMenu").innerHTML = genLangMenu();
+document.getElementById("cmpyMenu").innerHTML = genCmpyMenu();
+document.getElementById("locMenu").innerHTML = genLocMenu();
 
 function genLangMenu(){
-	var len = langJson.length;
 	var i = 0;
 	var retHTML = "";
 	var linkPrefix = "";
@@ -33,9 +57,34 @@ function genLangMenu(){
 }
 
 function genCmpyMenu(){
+	var i = 0;
+	var retHTML = "";
+	var linkPrefix = "";
+	if(state == 0)
+		linkPrefix += "company/";
 
+	for(i = 0; i<len; i++){
+		var dict = cmpyJson[i];
+		var cmpyName = dict["company_name"];
+		var cmpyAddr = cmpyName.replace(" ", "");
+		retHTML += "<li><a href="+linkPrefix+cmpyAddr+">"+cmpyName+"</a></li>"
+	}
+	return retHTML;
 }
 
 function genLocMenu(){
-	
+	var i = 0;
+	var retHTML = "";
+	var linkPrefix = "";
+	if(state == 0)
+		linkPrefix += "location/";
+
+	for(i = 0; i<len; i++){
+		var dict = locJson[i];
+		var locName = dict["location_name"];
+		var locAddr = locName.replace(" ", "");
+		locAddr = locAddr.replace(",", "");
+		retHTML += "<li><a href="+linkPrefix+locAddr+">"+locName+"</a></li>"
+	}
+	return retHTML;
 }
