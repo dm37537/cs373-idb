e677402817777dc18e3b270843a3306d291aec67
o0tomato0o <o0tomato0o@hotmail.com>
2015-04-08 16:08:29 -0500

    merged test_api.py to test.py.

--- tests.py
+++ tests.py
@@ -2,6 +2,10 @@ import unittest
 from programmerJobs import app
 from models import Job, Company, Location, Language, Skillset
 
+import json
+import requests
+import os
+
 class ProgrammerJobsTestCase(unittest.TestCase):
 	
 	def testCreatingJob(self):
@@ -138,6 +142,90 @@ class ProgrammerJobsTestCase(unittest.TestCase):
 		skillset.skillset_description = 'ipad'
 		assert skillset.skillset_name == 'IOS'
 		assert skillset.skillset_description == 'ipad'
+
+# Test languages
+class LanguageApiTest(unittest.TestCase):
+
+	# assuming we add our api onto our server
+	url =  "http://  **put_our_URL_here**   /api/"
+
+	# testing the whole set of Language data.
+	def test_lang(self):
+		response = requests.get(self.url + "languages/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the whole dataset in the languages
+		self.assertTrue(content == temp)
+
+	# testing a request to an individual language data
+	def test_lang_id(self):
+		response = requests.get(self.url + "languages/1/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the dataset of the language where id is 1
+		self.assertTrue(content == temp)
+
+	# testing the request when the data is not available
+	def test_lang_not_found(self):
+		response = requests.get(self.url + "languages/null/")
+		self.assertEqual(response.status_code, 404)
+
+
+# Test companies
+class CompanyApiTest(unittest.TestCase):
+
+	# assuming we add our api onto our server
+	url =  "http://  **put_our_URL_here**   /api/"
+
+	# testing the whole set of companies data.
+	def test_company(self):
+		response = requests.get(self.url + "companies/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the whole dataset in the companies
+		self.assertTrue(content == temp)
+
+	# testing a request to an individual company data
+	def test_company_id(self):
+		response = requests.get(self.url + "companies/1/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the dataset of the language where id is 1
+		self.assertTrue(content == temp)
+
+	# testing the request when the data is not available
+	def test_company_not_found(self):
+		response = requests.get(self.url + "companies/null/")
+		self.assertEqual(response.status_code, 404)
+
+
+# Test locations
+class LocationApiTest(unittest.TestCase):
+
+	# assuming we add our api onto our server
+	url =  "http://  **put_our_URL_here**   /api/"
+
+	# testing the whole set of locations data.
+	def test_location(self):
+		response = requests.get(self.url + "locations/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the whole dataset in the locations
+		self.assertTrue(content == temp)
+
+	# testing a request to an individual location data
+	def test_location_id(self):
+		response = requests.get(self.url + "locations/1/")
+		self.assertEqual(response.status_code, 200)
+		content = response.json()
+		temp = # the dataset of the language where id is 1
+		self.assertTrue(content == temp)
+
+	# testing the request when the data is not available
+	def test_location_not_found(self):
+		response = requests.get(self.url + "locations/null/")
+		self.assertEqual(response.status_code, 404)
+
 
 if __name__ == '__main__':
 	unittest.main()
